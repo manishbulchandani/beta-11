c46803900a7eeeae1b2f80d0b88c7aac0132a634
Dhruv Dixit <dhruvmes.8555@gmail.com>
2024-11-09 11:55:20 +0530

    getProfile API created & timelineNode controllers modified

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import userRoutes from "./user.routes"
+import timelineNodesRoutes from "./timelineNode.routes"
 
 const router = express.Router();
 
-router.use("/users",userRoutes)
+router.use("/users", userRoutes);
+
+router.use("/timelineNodes", timelineNodesRoutes);
 
 export default router;
--- server/src/routes/v1/timelineNode.routes.ts
+++ server/src/routes/v1/timelineNode.routes.ts
@@ -1,10 +1,13 @@
 import express from "express"
+import multer from "multer"
 // import * as authControllers from "../../controllers/auth/auth.controller"
 import * as timelineNodeControllers from "../../controllers/timelineNode.controller"
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 const router = express.Router();
 
-router.post('/addTimelineNode', timelineNodeControllers.handleAddTimelineNode);
+router.post('/addTimelineNode', upload.single('file'), timelineNodeControllers.handleAddTimelineNode);
 
 router.post('/getTimelineNodesGroupByTopic', timelineNodeControllers.handleGetTimelineNodeOrderByTopic);
 

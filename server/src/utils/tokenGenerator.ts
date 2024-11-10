d91e9169055cd4fc664a1a6191b810d5d7085a00
Manish <[manishbulchandani4@gmail.com]>
2024-11-10 11:07:28 +0530

    build error fix

--- server/src/utils/tokenGenerator.ts
+++ server/src/utils/tokenGenerator.ts
@@ -1,10 +1,9 @@
 import jwt from "jsonwebtoken";
-import { UserRole } from "../models/user.model";
 
 
 
-export const generateAccessToken = (userId:string, role:UserRole) => {
-  return jwt.sign({ userId, role }, process.env.JWT_SECRET_ACCESS!, {
+export const generateAccessToken = (userId:string) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET_ACCESS!, {
     expiresIn: process.env.JWT_ACCESS_EXPIRES,
   });
 };
